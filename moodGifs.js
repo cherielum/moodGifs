981edaf7ae22030cd13628f07f531701674b60df
Cherie Lum <charismatic.cherie@gmail.com>
2017-10-27 11:10:40 -0400

    adding files

---
+++ moodGifs.js
@@ -0,0 +1,58 @@
+function myFunction() {
+    document.body.style.backgroundColor = "orange";
+    	var x = document.createElement("IMG");
+	    x.setAttribute("src", "red.gif");
+	    x.setAttribute("width", "100");
+	    x.setAttribute("height", "100");
+	    document.body.appendChild(x);
+
+}
+
+function myFunction2() {
+    document.body.style.backgroundColor = "blue";
+    	var x = document.createElement("IMG");
+	    x.setAttribute("src", "happy2.gif");
+	    x.setAttribute("width", "100");
+	    x.setAttribute("height", "100");
+	    document.body.appendChild(x);
+
+}
+
+function myFunction3() {
+	document.body.style.backgroundColor = "green";
+		var x = document.createElement("IMG");
+	    x.setAttribute("src", "green.gif");
+	    x.setAttribute("width", "100");
+	    x.setAttribute("height", "100");
+	    document.body.appendChild(x);
+}
+
+function myFunction4() {
+	document.body.style.backgroundColor = "yellow";
+		var x = document.createElement("IMG");
+	    x.setAttribute("src", "sleepy.gif");
+	    x.setAttribute("width", "100");
+	    x.setAttribute("height", "100");
+	    document.body.appendChild(x);
+}
+
+function myFunction5() {
+	document.body.style.backgroundColor = "pink";
+		var x = document.createElement("IMG");
+	    x.setAttribute("src", "sad.gif");
+	    x.setAttribute("width", "150");
+	    x.setAttribute("height", "100");
+	    document.body.appendChild(x);
+}
+
+function myFunction6() {
+	document.body.style.backgroundColor = "white";
+		var x = document.createElement("IMG");
+	    x.setAttribute("src", "white.png");
+	    x.setAttribute("width", "100");
+	    x.setAttribute("height", "100");
+	    document.body.appendChild(x);
+}
+
+
+
